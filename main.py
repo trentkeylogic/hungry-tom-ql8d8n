d0edc42b070ecafd05267825066b1891c3f7d659
trentkeylogic <trent.keylogic@gmail.com>
2024-10-14 17:47:26 +0000

    ongoing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/bin/env python3
 
+import httpx
+import json
+import random
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,9 +10,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def index_root():
-  with open('index.html', 'r') as file:
-    return file.read()
+  return potion_page
+
+with open('index.html', 'r') as file:
+  potion_page=file.read()
 
 
-if __name__ == "__main__":
-  app.run()
+#if __name__ == "__main__":
+#  app.run()
+
+r = httpx.get('https://api.potterdb.com/v1/potions')
+potions=json.loads(r.text)['data']
+#print(r.status_code)
+#print(r.headers['content-type'])
+potion=potions[random.randrange(len(potions))]['attributes']
+print(potion['image'])
+print(potion['name'])
+print(potion['effect'])
+print(potion['ingredients'])
