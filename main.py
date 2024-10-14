96f55a0fad6897da5373a650ab9cef05a62f4550
trentkeylogic <trent.keylogic@gmail.com>
2024-10-14 15:31:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/bin/env python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index_root():
+  with open('index.html', 'r') as file:
+    return file.read()
+
+
+if __name__ == "__main__":
+  app.run()
