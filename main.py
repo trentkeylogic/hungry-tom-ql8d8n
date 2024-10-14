e247b3956e3e32d6b4f9cc537ffd25e92e865612
trentkeylogic <trent.keylogic@gmail.com>
2024-10-14 21:28:09 +0000

    working example

--- main.py
+++ main.py
@@ -7,23 +7,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def index_root():
-  return potion_page
+potions=json.loads(httpx.get('https://api.potterdb.com/v1/potions').text)['data']
 
 with open('index.html', 'r') as file:
   potion_page=file.read()
+  file.close()
 
+@app.route("/")
+def index_root():
+  #with open('index.html', 'r') as file:
+  #  potion_page=file.read()
+  #  file.close()
 
-#if __name__ == "__main__":
-#  app.run()
+  potion=potions[random.randrange(len(potions))]['attributes']
+  return potion_page.replace('potion_name', str(potion['name'])).replace('potion_image', str(potion['image'])).replace('potion_effect', str(potion['effect'])).replace('potion_ingredients', str(potion['ingredients']))
 
-r = httpx.get('https://api.potterdb.com/v1/potions')
-potions=json.loads(r.text)['data']
-#print(r.status_code)
-#print(r.headers['content-type'])
-potion=potions[random.randrange(len(potions))]['attributes']
+if __name__ == "__main__":
+  app.run()
+
+exit()
+
 print(potion['image'])
 print(potion['name'])
 print(potion['effect'])
